8e9669dd28ec73bd95641241bdb51835bee32bab
ruchikaroy <contactrroy.uk@gmail.com>
2025-10-27 12:20:12 +0000

    fetching users data via http request and displaying it on the page as a list

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,16 +1,41 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { CanceledError } from "axios";
+
+interface User {
+  name: string;
+  id: number;
+}
 
 function App() {
   let items = ["Delhi", "Mumbai", "Varanasi", "Pune"];
   const [alertVisible, setAlertVisibility] = useState(false);
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   const handleSelect = (item: string) => {
     console.log(item);
     window.open(`https://www.google.com/search?q=${item}`, "_blank");
   };
+  useEffect(() => {
+    const controller = new AbortController();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((error) => {
+        if (error instanceof CanceledError) {
+          return;
+        } else setError(error.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
   return (
     <>
       <ListGroup items={items} heading="City Names" onSelect={handleSelect} />
@@ -20,6 +45,14 @@ function App() {
       <Button color="primary" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button>
+      <div className="mt-4">
+        {error && <p className="text-danger">{error}</p>}
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
